1a608d6de75202ff4f53be38087fb8f87a2f6ee7
Micaela <141596396+mikuuki@users.noreply.github.com>
2024-03-10 19:05:25 -0300

    Add files via upload

--- miarchivo.js
+++ miarchivo.js
@@ -1,105 +1,70 @@
-let boton = document.getElementById("ingresar").addEventListener("click", respuestaClick)
-function respuestaClick() {
-  usuario.innerText = prompt("Ingresa Nombre y Apellido")
-  console.log(usuario.innerText)
-  total.innerText = Number(prompt("Ingresa el monto a abonar"))
-  console.log(total.innerText)
-  localStorage.setItem("usuario", respuestaClick)
-};
-
-
-
-let formfecha = document.getElementById("fecha");
-const fecha = [];
-for (const fechaAgregada of fecha) {
-  let li = document.createElement("li");
-  li.innerHTML = fechaAgregada
-  formfecha.appendChild(li);
-
+function ingresar() {
+    let usuarioNew = document.getElementById("usuarioNew").value;
+    let dniNew = document.getElementById("dniNew").value;
+    localStorage.setItem("nombreIngresado", usuarioNew);
+    localStorage.setItem("dniIngresado", dniNew);
+    return false;
 }
-let formdescripcion = document.getElementById("descripcion");
-const descripcion = [];
-for (const descripcionAgregada of descripcion) {
-  let li = document.createElement("li");
-  li.innerHTML = descripcionAgregada
-  formdescripcion.appendChild(li);
-
-}
-let formmonto = document.getElementById("monto");
-const monto = [];
-for (const montoAgregado of monto) {
-  let li = document.createElement("li");
-  li.innerHTML = montoAgregado
-  formmonto.appendChild(li);
-
-}
+let usuario = document.getElementById("usuarioIngresado");
+usuario.innerHTML = localStorage.getItem("nombreIngresado");
+let dni = document.getElementById("dniIngresado")
+dni.innerHTML = localStorage.getItem("dniIngresado");
 
 
+//--------------------------------------------------------------------
 
 
-let boton2 = document.getElementById("btn1").addEventListener("click", respuestaClick2)
-function respuestaClick2() {
-  let nuevaFecha = formfecha;
-  let li = document.createElement("li");
-  li.innerHTML = nuevaFecha;
-  formfecha.appendChild(li);
+let btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click", cargar);
+//Agregar Gastos
+let gastos = [];
+function cargar() {
+    let nombre = document.getElementById("nombreGastos").value;
+    let importe = document.getElementById("importeGastos").value;
+    let fecha = document.getElementById("fechaGastos").value;
 
+    if (nombre && importe && fecha) {
+        let nuevoGasto = {
+            nombre: nombre,
+            importe: importe,
+            fecha: fecha
+        };
+        gastos.push(nuevoGasto);
+        limpiarCampos();
+        actualizarLista();
+            } else {
 
-  let nuevaDescrip = formdescripcion;
-  let li2 = document.createElement("li");
-  li2.innerHTML = nuevaDescrip;
-  formdescripcion.appendChild(li2);
-
-
-  let nuevoMonto = formmonto;
-  let li3 = document.createElement("li");
-  li3.innerHTML = nuevoMonto;
-  formmonto.appendChild(li3);
-
-  Toastify({
-    text: "Gastos agregados",
-    duration: 5000,
-    style: {background: 'linear-gradient(to right, #000000, #97989a)'}}).showToast();
-
-
-
-  formfecha.push(li);
-  formdescripcion.push(li);
-  formmonto.push(li);
-}
-
-console.log(fecha.length)
-console.log(descripcion.length)
-console.log(monto.length)
-
-let boton3 = document.getElementById("btn2").addEventListener("click", respuestaClick3)
-function respuestaClick3() {
-  Swal.fire({
-    title: "¿Abonás total registrado?",
-    text: "No podrás revertirlo!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Si, pagar!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Ya abonaste!",
-        text: "Tus gastos fueron abonados",
-        icon: "success"
-      });
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Agregado",
+            showConfirmButton: false,
+            width: 220,
+            timer: 900
+        });
     }
-  });
-
-  localStorage.clear()
-
-
 }
-const detalle = [{ fecha }, { descripcion }, { monto }];
-function guardarLocal() {
-  localStorage.setItem("gastos", detalle)
-};
-guardarLocal("listaGastos", JSON.stringify(detalle));
+function limpiarCampos() {
+    document.getElementById("nombreGastos").value = "";
+    document.getElementById("importeGastos").value = "";
+    document.getElementById("fechaGastos").value = "";
+}
+function actualizarLista() {
+    let listaDeGastos = document.getElementById("listaDeGastos");
+
+    let total = 0;
+    listaDeGastos.innerHTML = "";
+    gastos.forEach(gasto => {
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = `${gasto.nombre.toUpperCase()} - $${gasto.importe} - ${gasto.fecha}`;
+        listaDeGastos.appendChild(nuevoElemento);
+        total += parseFloat(gasto.importe);
+    });
+    let totalElement = document.getElementById("total");
+    totalElement.textContent = `Total $${total.toFixed(2)}`;
+}
+
+
+
 
 
